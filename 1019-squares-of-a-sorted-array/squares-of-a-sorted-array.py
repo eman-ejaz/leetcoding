ffdf3dccfa96f635a8d53ceb5af611a0c7f801b5
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2024-03-14 18:23:22 +1100

    Time: 155 ms (56.36%) | Memory: 18.9 MB (34.37%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,19 +1,31 @@
 class Solution:
     def sortedSquares(self, arr: List[int]) -> List[int]:
-        result = [0 for _ in range(len(arr))]
+        # result = [0 for _ in range(len(arr))]
 
-        left, right = 0, len(arr) - 1
-        index = len(result) - 1
+        # left, right = 0, len(arr) - 1
+        # index = len(result) - 1
 
-        while left <= right:
-            if math.pow(arr[left], 2) < math.pow(arr[right], 2):
-                result[index] = int(math.pow(arr[right], 2))
-                index -= 1
-                right -= 1
+        # while left <= right:
+        #     if math.pow(arr[left], 2) < math.pow(arr[right], 2):
+        #         result[index] = int(math.pow(arr[right], 2))
+        #         index -= 1
+        #         right -= 1
+        #     else:
+        #         result[index] = int(math.pow(arr[left], 2))
+        #         index -= 1
+        #         left += 1
+        
+        # return result
+        l, r = 0, len(arr) - 1
+
+        result = [0] * len(arr)
+
+        for i in range(len(result) - 1, -1, -1):
+            if arr[l] ** 2 > arr[r] ** 2:
+                result[i] = arr[l] ** 2
+                l += 1
             else:
-                result[index] = int(math.pow(arr[left], 2))
-                index -= 1
-                left += 1
-        
+                result[i] = arr[r] ** 2
+                r -= 1
+
         return result
-        
