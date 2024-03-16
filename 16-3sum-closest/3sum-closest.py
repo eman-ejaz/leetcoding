6119b619d93018bc7cf7209bf666245cc3cdedd8
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2024-03-17 01:24:16 +1100

    Time: 390 ms (64.85%) | Memory: 16.6 MB (62.74%) - LeetSync

--- 16-3sum-closest/3sum-closest.py
+++ 16-3sum-closest/3sum-closest.py
@@ -1,7 +1,30 @@
 class Solution:
     def threeSumClosest(self, arr: List[int], target_sum: int) -> int:
+        # arr.sort()
+        # result = float('inf')
+        # for i in range(len(arr) - 2):
+        #     left, right = i + 1, len(arr) - 1
+
+        #     while left < right:
+        #         target_diff = target_sum - arr[i] - arr[left] - arr[right]
+
+        #         if target_diff == 0:
+        #             return arr[i] + arr[left] + arr[right]
+
+        #         if abs(target_diff) < abs(result):
+        #             result = target_diff
+
+        #         if target_diff > 0:
+        #             left += 1
+        #         else:
+        #             right -= 1
+
+        # return target_sum - result
+            
+
         arr.sort()
         result = float('inf')
+
         for i in range(len(arr) - 2):
             left, right = i + 1, len(arr) - 1
 
@@ -9,7 +32,7 @@ class Solution:
                 target_diff = target_sum - arr[i] - arr[left] - arr[right]
 
                 if target_diff == 0:
-                    return arr[i] + arr[left] + arr[right]
+                    return target_sum - target_diff
 
                 if abs(target_diff) < abs(result):
                     result = target_diff
@@ -20,4 +43,3 @@ class Solution:
                     right -= 1
 
         return target_sum - result
-            
