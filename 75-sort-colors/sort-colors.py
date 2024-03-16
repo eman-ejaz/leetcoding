456489182866a269f8098befa48ac986e097423f
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2024-03-17 02:58:27 +1100

    Time: 31 ms (91.26%) | Memory: 16.5 MB (47.13%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sortColors(self, arr: List[int]) -> None:
+        if len(arr) < 2:
+            return arr
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left, right = 0, len(arr) - 1
+
+        i = 0   
+
+        while i <= right:
+            if arr[i] == 0:
+                arr[left], arr[i] = arr[i], arr[left]
+                left += 1
+            elif arr[i] == 2:
+                arr[right], arr[i] = arr[i], arr[right]
+                right -= 1
+                i -= 1
+        
+            i += 1
+        return arr
+        
