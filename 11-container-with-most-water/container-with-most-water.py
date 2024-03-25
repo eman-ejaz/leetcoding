fbde16fb3764eb293bfdc5dea4c9f6e1af8f536f
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2024-03-25 23:42:14 +1100

    Time: 540 ms (23.89%) | Memory: 30.2 MB (6.98%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,21 +1,76 @@
 class Solution:
     def maxArea(self, arr: List[int]) -> int:
-        if len(arr) < 2:
-            return -1
-        
-        result_area = float('-inf')
+        l, r = 0, len(arr) - 1
+        max_area = float('-inf')
+        while l < r:
+            area = (r - l) * min(arr[l], arr[r])
+            max_area = max(max_area, area)
 
-        right = len(arr) - 1
-        left = 0
-
-        while left < right:
-            temp_area = (right - left) * min(arr[left], arr[right])
-            result_area = max(result_area, temp_area)
-
-            if arr[left] < arr[right]:
-                left += 1
+            if arr[l] < arr[r]:
+                l += 1
             else:
-                right -= 1
-        return result_area
+                r -= 1
+
+        return max_area
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # if len(arr) < 2:
+        #     return -1
+        
+        # result_area = float('-inf')
+
+        # right = len(arr) - 1
+        # left = 0
+
+        # while left < right:
+        #     temp_area = (right - left) * min(arr[left], arr[right])
+        #     result_area = max(result_area, temp_area)
+
+        #     if arr[left] < arr[right]:
+        #         left += 1
+        #     else:
+        #         right -= 1
+        # return result_area
 
         
