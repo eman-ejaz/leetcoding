b01cf6ff4903a4e758c6e968a38d624c63f68209
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2024-03-28 01:28:25 +1100

    Time: 39 ms (46.18%) | Memory: 16.5 MB (90.09%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,24 +1,42 @@
 class Solution:
     def sortColors(self, arr: List[int]) -> None:
-        result = []
-        nums_freq = {}
+        # result = []
+        # nums_freq = {}
 
-        for num in arr:
-            nums_freq[num] = nums_freq.get(num, 0) + 1
+        # for num in arr:
+        #     nums_freq[num] = nums_freq.get(num, 0) + 1
 
-        for i in range(len(arr)):
-            if 0 in nums_freq and nums_freq[0] > 0:
-                arr[i] = 0
-                nums_freq[0] -= 1
-            elif 1 in nums_freq and nums_freq[1] > 0:
-                arr[i] = 1
-                nums_freq[1] -= 1
+        # for i in range(len(arr)):
+        #     if 0 in nums_freq and nums_freq[0] > 0:
+        #         arr[i] = 0
+        #         nums_freq[0] -= 1
+        #     elif 1 in nums_freq and nums_freq[1] > 0:
+        #         arr[i] = 1
+        #         nums_freq[1] -= 1
+        #     else:
+        #         arr[i] = 2
+
+        # print(arr)
+
+        # return result
+
+
+        l, r = 0, len(arr) - 1
+
+        i = 0
+
+        while i <= r:
+            if arr[i] == 0:
+                arr[i], arr[l] = arr[l], arr[i]
+                i += 1
+                l += 1
+            elif arr[i] == 2:
+                arr[i], arr[r] = arr[r], arr[i]
+                r -= 1
             else:
-                arr[i] = 2
-
-        print(arr)
-
-        return result
+                i += 1
+        
+        return arr
 
 
 
