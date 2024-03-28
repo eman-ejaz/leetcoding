0b9ac3a3b3fe415f36117db4faa05ea9fa47f3d4
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2024-03-29 02:44:19 +1100

    Time: 43 ms (9.21%) | Memory: 16.5 MB (43.45%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -2,38 +2,101 @@ class Solution:
     def backspaceCompare(self, str1: str, str2: str) -> bool:
         p1, p2 = len(str1) - 1, len(str2) - 1
 
-
         while p1 >= 0 or p2 >= 0:
-            np1 = self.remove_bkspc(p1, str1)
-            np2 = self.remove_bkspc(p2, str2)
+            updated_pointer1 = self.remove_bkspc(p1, str1)
+            updated_pointer2 = self.remove_bkspc(p2, str2)
 
-            if np1 < 0 and np2 < 0:
+            if updated_pointer1 < 0 and updated_pointer2 < 0:
                 return True
 
-            if np1 < 0 or np2 < 0:
+            if updated_pointer1 < 0 or updated_pointer2 < 0:
                 return False
 
-            if str1[np1] != str2[np2]:
+            if str1[updated_pointer1] != str2[updated_pointer2]:
                 return False
 
-            p1 = np1 - 1
-            p2 = np2 - 1
+            p1 = updated_pointer1 - 1
+            p2 = updated_pointer2 - 1
         
         return True
 
+    
     def remove_bkspc(self, index, s):
-        num_bkspc = 0
+        bkspc_counter = 0
+
         while index >= 0:
             if s[index] == '#':
-                num_bkspc += 1
-            elif num_bkspc > 0:
-                num_bkspc -= 1
+                bkspc_counter += 1
+            elif bkspc_counter > 0:
+                bkspc_counter -= 1
             else:
                 break
-
+            
             index -= 1
         
         return index
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #     p1, p2 = len(str1) - 1, len(str2) - 1
+
+
+    #     while p1 >= 0 or p2 >= 0:
+    #         np1 = self.remove_bkspc(p1, str1)
+    #         np2 = self.remove_bkspc(p2, str2)
+
+    #         if np1 < 0 and np2 < 0:
+    #             return True
+
+    #         if np1 < 0 or np2 < 0:
+    #             return False
+
+    #         if str1[np1] != str2[np2]:
+    #             return False
+
+    #         p1 = np1 - 1
+    #         p2 = np2 - 1
+        
+    #     return True
+
+    # def remove_bkspc(self, index, s):
+    #     num_bkspc = 0
+    #     while index >= 0:
+    #         if s[index] == '#':
+    #             num_bkspc += 1
+    #         elif num_bkspc > 0:
+    #             num_bkspc -= 1
+    #         else:
+    #             break
+
+    #         index -= 1
+        
+    #     return index
 
 
 
