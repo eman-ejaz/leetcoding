1d3e9ec382b8fc71f400ceeead7b67846606c1b3
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2024-10-08 00:44:11 +1000

    Time: 535 ms (30.19%), Space: 29.5 MB (67.45%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,5 +1,19 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+
+        # brute force
+
+        # max_a = 0
+
+        # for i in range(len(height)):
+        #     for j in range(i + 1, len(height)):
+        #         area = (j - i) * (min(height[j], height[i]))
+        #         max_a = max(max_a, area)
+
+        # return max_a
+
+
+        # linear time solution
         l, r = 0, len(height) - 1
 
         max_area = float('-inf')
