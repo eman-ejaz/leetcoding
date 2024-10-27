3247890be634a662bb9b049c10aac0f8faad4551
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2024-10-27 22:10:01 +1100

    Time: 0 ms (100%), Space: 16.6 MB (12.49%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,7 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        l, r, i = 0, len(nums) - 1, 0
+        i, l, r = 0, 0, len(nums) - 1
 
         while i <= r:
             if nums[i] == 0:
