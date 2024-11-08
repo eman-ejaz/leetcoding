5766a8b5f8688538a5f9d952e4e45bb56d91bbf9
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2024-11-09 01:09:46 +1100

    Time: 31 ms (46%), Space: 16.8 MB (8.12%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,31 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        freq = Counter(s1)
+
+        print(freq)
+
+        matched = 0
+
+        ws = 0
+
+        for i in range(len(s2)):
+            if s2[i] in freq:
+                freq[s2[i]] -= 1
+
+                if freq[s2[i]] == 0:
+                    matched += 1
+
+                
+                if matched == len(freq):
+                    return True
+
+            if i - ws >= len(s1) - 1:
+                if s2[ws] in freq:
+                    if freq[s2[ws]] == 0:
+                        matched -= 1
+                    freq[s2[ws]] += 1
+
+                ws += 1
+
+        return False
+        
