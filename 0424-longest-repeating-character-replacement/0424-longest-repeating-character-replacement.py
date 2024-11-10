c81dd581956fbfee3219f2e9e85a32bb96ebc001
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2024-11-11 00:39:14 +1100

    Time: 47 ms (74.19%), Space: 16.7 MB (21.44%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,5 +1,57 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        freq = {}
+
+        mc = 0
+
+        ws = 0
+
+        res = float('-inf')
+
+        for i in range(len(s)):
+            freq[s[i]] = freq.get(s[i], 0) + 1
+
+            mc = max(mc, freq[s[i]])
+
+            while (i - ws + 1) - mc > k:
+                freq[s[ws]] -= 1
+
+                ws += 1
+
+            res = max(res, i - ws + 1)
+
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         # cf = [0] * 26
 
         # ws = 0
@@ -24,26 +76,26 @@ class Solution:
         # return max_length
 
 
-        count = {}
+        # count = {}
 
-        ws = 0
-        max_length = float('-inf')
-        max_char = 0
+        # ws = 0
+        # max_length = float('-inf')
+        # max_char = 0
 
-        for i in range(len(s)):
-            count[s[i]] = 1 + count.get(s[i], 0)
+        # for i in range(len(s)):
+        #     count[s[i]] = 1 + count.get(s[i], 0)
 
-            max_char = max(max_char, count[s[i]])
+        #     max_char = max(max_char, count[s[i]])
 
 
-            while (i - ws + 1) - max_char > k:
-                count[s[ws]] -= 1 
-                ws += 1
+        #     while (i - ws + 1) - max_char > k:
+        #         count[s[ws]] -= 1 
+        #         ws += 1
 
-            max_length = max(max_length, i - ws + 1)
+        #     max_length = max(max_length, i - ws + 1)
             
 
-        return max_length
+        # return max_length
 
 
 
