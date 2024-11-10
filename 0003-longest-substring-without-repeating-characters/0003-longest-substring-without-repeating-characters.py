8ea9a3dc3db9d677040e7cce4df6a03eaa1390d4
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2024-11-10 23:57:11 +1100

    Time: 27 ms (24.55%), Space: 16.9 MB (7.72%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -2,24 +2,44 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         # brute force
 
+        # if len(s) == 0:
+        #     return 0
+
+        # max_len = float('-inf')
+        # freq = {}
+
+        # for i in range(len(s)):
+        #     freq[s[i]] = 1 + freq.get(s[i], 0)
+        #     for j in range(i + 1, len(s)):
+        #         if s[j] in freq:
+        #             break
+        #         else:
+        #             freq[s[j]] = 1 + freq.get(s[j], 0)
+        #     max_len = max(max_len, len(freq))
+        #     freq = {}
+            
+
+        # return max_len
+
+
         if len(s) == 0:
             return 0
-
-        max_len = float('-inf')
         freq = {}
+
+        ws = 0
+        max_len = float('-inf')
 
         for i in range(len(s)):
             freq[s[i]] = 1 + freq.get(s[i], 0)
-            for j in range(i + 1, len(s)):
-                if s[j] in freq:
-                    break
-                else:
-                    freq[s[j]] = 1 + freq.get(s[j], 0)
-            max_len = max(max_len, len(freq))
-            freq = {}
-            
 
+            while freq[s[i]] > 1:
+                freq[s[ws]] -= 1
+                ws += 1
+
+            max_len = max(max_len, i - ws + 1)
+            
         return max_len
+
 
 
         
