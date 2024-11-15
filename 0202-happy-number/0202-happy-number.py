e586dcdd4717252be65fc90e54cbf18d92664b83
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2024-11-16 00:51:24 +1100

    Time: 3 ms (44.46%), Space: 16.6 MB (24.87%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        if n == 1:
+            return True
+            
+        def cal_squares_sum(num):
+            s = 0
+            while num > 0:
+                s += (num % 10) ** 2
+                num //= 10
+
+            return s
+
+        f, s = n, n
+
+        while True:
+            s = cal_squares_sum(s)
+            f = cal_squares_sum(cal_squares_sum(f))
+
+            if f == 1:
+                return True
+
+            if s == f:
+                return False
+            
+
+        
