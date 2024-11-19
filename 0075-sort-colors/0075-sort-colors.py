fec891f7a169efd428ae2662c5746ac8378f454f
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2024-11-19 13:12:06 +1100

    Time: 0 ms (100%), Space: 16.8 MB (6.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -5,6 +5,59 @@ class Solution:
         """
         i, l, r = 0, 0, len(nums) - 1
 
+
+        while i <= r:
+            if nums[i] == 0:
+                nums[i], nums[l] = nums[l], nums[i]
+                i += 1
+                l += 1
+            elif nums[i] == 2:
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+            else:
+                i += 1
+        
+        return nums
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        i, l, r = 0, 0, len(nums) - 1
+
         while i <= r:
             if nums[i] == 0:
                 nums[i], nums[l] = nums[l], nums[i]
