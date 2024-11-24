cebdd92542dfba0d89f1c73e2228a6c8d1942fe6
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2024-11-24 18:45:39 +1100

    Time: 40 ms (42.85%), Space: 17.2 MB (99.22%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -11,50 +11,80 @@ class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return None
-        dummy = Node(0)
-        curr = head
-        temp = dummy
 
-        org_node_map = {}
-        new_node_map = {}
-        counter = 1
+
+        oldToClone = {None: None}
+        dummy = Node(0)
+
+        temp = dummy
+        curr = head
 
         while curr:
             temp.next = Node(curr.val)
+            oldToClone[curr] = temp.next
 
-            org_node_map[curr] = counter
-            arr = new_node_map.get(curr.val, [])
-            arr.append((counter, temp.next))
-            new_node_map[curr.val] = arr
-            temp = temp.next
             curr = curr.next
-
-            counter += 1
+            temp = temp.next
         
-        temp_clone = dummy.next
-        temp_head = head
+        curr = head
+        temp = dummy.next
 
-        print(org_node_map)
+        while curr:
+            clone_random = oldToClone[curr.random]
+            temp.random = clone_random
 
-        while temp_head:
-            if not temp_head.random:
-                temp_clone.random = temp.next
-            else:
-                random = temp_head.random
-                random_index = org_node_map[random]
-
-                clone_nodes = new_node_map[random.val]
-
-                for n in clone_nodes:
-                    index, node = n
-                    if index == random_index:
-                        temp_clone.random = node
-                        break
-
-            temp_head = temp_head.next
-            temp_clone = temp_clone.next
+            curr = curr.next
+            temp = temp.next
 
         return dummy.next
+
+
+
+
+        # dummy = Node(0)
+        # curr = head
+        # temp = dummy
+
+        # org_node_map = {}
+        # new_node_map = {}
+        # counter = 1
+
+        # while curr:
+        #     temp.next = Node(curr.val)
+
+        #     org_node_map[curr] = counter
+        #     arr = new_node_map.get(curr.val, [])
+        #     arr.append((counter, temp.next))
+        #     new_node_map[curr.val] = arr
+        #     temp = temp.next
+        #     curr = curr.next
+
+        #     counter += 1
+        
+        # temp_clone = dummy.next
+        # temp_head = head
+
+        # print(org_node_map)
+
+        # while temp_head:
+        #     if not temp_head.random:
+        #         temp_clone.random = temp.next
+        #     else:
+        #         random = temp_head.random
+        #         random_index = org_node_map[random]
+
+        #         clone_nodes = new_node_map[random.val]
+
+        #         for n in clone_nodes:
+        #             index, node = n
+        #             if index == random_index:
+        #                 temp_clone.random = node
+        #                 break
+
+        #     temp_head = temp_head.next
+        #     temp_clone = temp_clone.next
+
+        # return dummy.next
                 
             
 
