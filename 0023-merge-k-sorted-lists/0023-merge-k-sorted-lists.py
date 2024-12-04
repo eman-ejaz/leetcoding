0e11f65d8891c898246b9fe4e8f045342dd0557a
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2024-12-05 00:16:53 +1100

    Time: 39 ms (19.31%), Space: 20.6 MB (6.25%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -27,30 +27,22 @@ class Solution:
 
         while l1 and l2:
             if l1.val < l2.val:
-                print('l1', l1.val)
                 temp = ListNode(l1.val)
                 res.next = temp
                 res = res.next
 
                 l1 = l1.next
             else:
-                print('l2', l2.val)
                 temp = ListNode(l2.val)
                 res.next = temp
                 res = res.next
 
                 l2 = l2.next
 
-        while l1:
-            node = ListNode(l1.val)
-            res.next = node
-            res = res.next
-            l1 = l1.next
-        while l2:
-            node = ListNode(l2.val)
-            res.next = node
-            res = res.next
-            l2 = l2.next
+        if l1:
+            res.next = l1
+        if l2:
+            res.next = l2
 
         return dummy.next
 
