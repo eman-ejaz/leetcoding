513c8537883e98ff9c4675cbe3b019bc0818fa67
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2024-12-04 23:33:38 +1100

    Time: 128 ms (71.86%), Space: 77.6 MB (50.58%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,5 +1,5 @@
 class LinkedList:
-    def __init__(self, key, val, next = None, prev = None):
+    def __init__(self, key, val, next=None, prev=None):
         self.val = val
         self.key = key
         self.next = next
@@ -7,78 +7,55 @@ class LinkedList:
 
 
 class LRUCache:
-
     def __init__(self, capacity: int):
-        self.head = LinkedList(0, 0)
-        self.tail = LinkedList(0, 0)
-
+        self.head = LinkedList(0, 0)  # Dummy head
+        self.tail = LinkedList(0, 0)  # Dummy tail
         self.head.next = self.tail
         self.tail.prev = self.head
-
         self.list_map = {}
         self.capacity = capacity
 
     def remove(self, node):
+        """Remove a node from its current position."""
         tprev = node.prev
         tnext = node.next
+        tprev.next = tnext
+        tnext.prev = tprev
 
-        node.prev.next = tnext
-        node.next.prev = tprev
-    
     def add_to_front(self, node):
+        """Add a node right after the head."""
         tnext = self.head.next
-
         self.head.next = node
         node.prev = self.head
         node.next = tnext
         tnext.prev = node
-        
+
     def get(self, key: int) -> int:
         if key not in self.list_map:
             return -1
         node = self.list_map[key]
 
-        val = node.val
-
+        # Move the accessed node to the front
         self.remove(node)
         self.add_to_front(node)
 
-        self.list_map[key] = node
-
-        return val
-        
+        return node.val
 
     def put(self, key: int, value: int) -> None:
-        if (key in self.list_map):
+        if key in self.list_map:
+            # Update value and move node to front
             node = self.list_map[key]
-
             node.val = value
-
             self.remove(node)
             self.add_to_front(node)
-            
         else:
-            if len(self.list_map) < self.capacity:
-                node = LinkedList(key, value)
-                self.list_map[key] = node
+            if len(self.list_map) >= self.capacity:
+                # Remove the least recently used (LRU) node
+                lru_node = self.tail.prev
+                self.remove(lru_node)
+                del self.list_map[lru_node.key]
 
-                self.add_to_front(node)
-            else:
-                del self.list_map[self.tail.prev.key]
-                self.remove(self.tail.prev)
-
-                node = LinkedList(key, value)
-                self.add_to_front(node)
-                self.list_map[key] = node
-            
-            
-           
-
-                
-            
-
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+            # Add the new node
+            new_node = LinkedList(key, value)
+            self.add_to_front(new_node)
+            self.list_map[key] = new_node
