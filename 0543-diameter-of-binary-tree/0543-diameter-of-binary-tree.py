05097579cba8e08a5acc3e2b196a0ae0529363e7
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2025-05-30 00:53:40 +1000

    Time: 3 ms (94.36%), Space: 21 MB (10.8%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -5,7 +5,95 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    res = 0
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
+            if not node:
+                return 0
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            self.res = max(self.res, left + right)
+
+            return 1 + max(left, right)
+
+        dfs(root)
+
+        return self.res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # maxi = [0]
+
+        # def find_longest(node):
+        #     if not node:
+        #         return 0
+
+        #     left = find_longest(node.left)
+        #     right = find_longest(node.right)
+
+        #     return 1 + max(left,  right)
+
+        # def dfs(node):
+        #     if not node:
+        #         return
+
+        #     left = find_longest(node.left)
+        #     right = find_longest(node.right)
+        #     maxi[0] = max(maxi[0], left + right)
+
+        #     dfs(node.left)
+        #     dfs(node.right)
+
+        
+        # dfs(root)
+        # return maxi[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         maxi = [0]
         def dfs(node):
             if not node:
